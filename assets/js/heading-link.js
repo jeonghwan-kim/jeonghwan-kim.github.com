4ecfc738e4ad49a60eabb52a8c7148799f5cc6a2
jeonghwan-kim <ej88ej@gmail.com>
2016-10-15 23:21:35 +0900

    update heading-link module

--- assets/js/heading-link.js
+++ assets/js/heading-link.js
@@ -1,11 +1,35 @@
-function headingLink(document, headings) {
-  var headings = $(document).find(headings[0])
-  $.each(headings, function (idx, heading) {
-    $(heading).prepend('<a class="heading-link" style="left: float; margin-left: -20px; padding-right: 7px; display: none;" href="#' + heading.id + '">#</a>')
-    $(heading).hover(function () {
-      $($(heading).find('a')[0]).css('display', 'inline-block');
-    }, function () {
-      $($(heading).find('a')[0]).css('display', 'none');
-    });
-  });
+/**
+ * headings에 내부 링크를 생성
+ *
+ * @param document 문서 최상단 클래스명 (post-content)
+ * @param headings 헤딩 태그명 (예: "h2")
+ */
+function headingLink(documentClassName, headingTagName) {
+  documentClassName = documentClassName.replace('.', '');
+
+  var doc = document.getElementsByClassName(documentClassName)[0],
+      headings = doc.getElementsByTagName(headingTagName),
+      i,
+      h,
+      len = headings.length;
+
+  for (i = 0; i < len; i++) {
+    h = headings[i];
+    h.innerHTML = '<a class="heading-link" style="float:left; margin-left:-20px; padding-right:7px; display:none;"  href="#' + h.id + '">#</a>'
+        + h.textContent;
+
+    h.addEventListener('mouseover', (function (h) {
+      return function () {
+        h.getElementsByTagName('a')[0].style.display = 'block';
+      };
+    })(h));
+
+
+    h.addEventListener('mouseout', (function (h) {
+      return function () {
+        h.getElementsByTagName('a')[0].style.display = 'none';
+      }
+    })(h));
+
+  }
 }
