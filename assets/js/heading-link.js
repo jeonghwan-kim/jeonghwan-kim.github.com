9dce39b5ccda6bcbd03b509fb3e54785df6d9c88
김정환 <6pack@woowahan.com>
2017-02-21 21:06:51 +0900

    cagetory clock handler

--- assets/js/heading-link.js
+++ assets/js/heading-link.js
@@ -9,3 +9,17 @@ function headingLink(document, headings) {
     });
   });
 }
+
+(function(document) {
+  function toggle(target) {
+    if (!target) return;
+    target.style.display = target.style.display === 'block' ? 'none' : 'block';
+  }
+  document.addEventListener('DOMContentLoaded', function(e) {
+    var ctgyBtn = document.getElementsByClassName('category-menu-icon')[0];
+    if (!ctgyBtn) return;
+    ctgyBtn.onclick = function() {
+      toggle(document.getElementsByClassName('trigger')[0]);
+    };
+  });
+})(document);
