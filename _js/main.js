ac32f30ebfcd91ac6929b9188901746a276de201
김정환 <6pack@madup.com>
2018-09-15 18:13:53 +0900

    태그 트레킹 로직 추가

--- _js/main.js
+++ _js/main.js
@@ -1,5 +1,3 @@
-"use strict";
-
 import googleAnalytics from './googleAnalytics';
 
 const tag = '[app]'
@@ -7,6 +5,20 @@ const tag = '[app]'
 const onload = () => {
   console.log(tag, 'DOMContentLoaded')
   googleAnalytics.init(window, document)
+
+  const post = document.querySelector('#post')
+  const postList = document.querySelector('#post-list')
+  if (post || postList) {
+    const el = post || postList
+    el.addEventListener('click', evt => {
+      googleAnalytics.sendEvent({
+        category: 'tag',
+        action: 'click',
+        label: post ? 'in post' : 'in post list',
+        value: evt.target.dataset.gaValue
+      })
+    })
+  }
 };
 
 document.addEventListener('DOMContentLoaded', onload);
