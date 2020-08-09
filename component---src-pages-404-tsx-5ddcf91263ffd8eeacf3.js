7ec4c023181070c11dce6c0bf06e0f6e80be2986
김정환 <ej88ej@gmail.com>
2020-08-09 12:40:59 +0900

    first commit

---
+++ component---src-pages-404-tsx-5ddcf91263ffd8eeacf3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"i6+/":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),u=n.n(a),l=n("B7F5"),o=n("H8eV");t.default=function(){return u.a.createElement(l.a,null,u.a.createElement(o.a,{title:"404: Not found"}),u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-5ddcf91263ffd8eeacf3.js.map
