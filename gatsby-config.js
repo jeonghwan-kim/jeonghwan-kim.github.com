a0fd09c409434975b7380ddfb97ed6112deca3b0
김정환 <ej88ej@gmail.com>
2020-08-19 11:10:14 +0900

    gatsby-plugin-react-helmet 활성화

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-sass`,
-    // `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
