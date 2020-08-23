02eae0d8317ce8faff16f1f1536058abdaec4a7b
김정환 <ej88ej@gmail.com>
2020-08-23 22:32:33 +0900

    layout에 있던 코드를 primsjs-copy 훅 으로 분리

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -5,15 +5,12 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React, { FC, useState, useEffect, ReactNode } from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import Clipboard from "clipboard"
-
-import Header from "./header"
-import Footer from "./footer"
-
-import layoutStore from "./layout-store"
+import { graphql, useStaticQuery } from "gatsby"
+import React, { FC, ReactNode, useEffect, useState } from "react"
 import "../../sass/main.scss"
+import Footer from "./footer"
+import Header from "./header"
+import layoutStore from "./layout-store"
 import "./layout.scss"
 
 interface P {
@@ -43,48 +40,6 @@ const Layout: FC<P> = p => {
     layoutStore.on("modalChanged", modals => {
       setModals([...modals])
     })
-
-    //
-    // al pre tags on the page
-    const pres = document.getElementsByTagName("pre")
-
-    //
-    // reformat html of pre tags
-    if (pres !== null) {
-      for (let i = 0; i < pres.length; i++) {
-        // check if its a pre tag with a prism class
-        if (isPrismClass(pres[i])) {
-          // insert code and copy element
-          pres[
-            i
-          ].innerHTML = `<button class="btn btn-secondary copy">복사</button>${pres[i].innerHTML}`
-        }
-      }
-    }
-
-    //
-    // create clipboard for every copy element
-    const clipboard = new Clipboard(".copy", {
-      target: trigger => {
-        return trigger.nextElementSibling
-      },
-    })
-
-    //
-    // do stuff when copy is clicked
-    clipboard.on("success", event => {
-      event.trigger.textContent = "복사함!"
-      setTimeout(() => {
-        event.clearSelection()
-        event.trigger.textContent = "복사"
-      }, 2000)
-    })
-
-    //
-    // helper function
-    function isPrismClass(preTag) {
-      return preTag.className.substring(0, 8) === "language"
-    }
   }, [])
 
   const { author, title, social } = data.site.siteMetadata
