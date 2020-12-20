16d7923579768d9f7d3979a5ac1ee55e724f9fd8
김정환 <jh.kim@woowahan.com>
2020-12-21 07:50:43 +0900

    Updates

---
+++ component---src-pages-404-tsx-ad04b7baa126c6a80163.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"i6+/":function(e,a,t){"use strict";t.r(a);var n=t("q1tI"),l=t.n(n),r=t("Nj58"),c=t("Ac47"),o=t("H8eV");a.default=function(){return l.a.createElement(c.a,{hasHeaderBorder:!0},l.a.createElement(o.a,{title:"404: Not found"}),l.a.createElement(r.b,{className:"container-sm"},l.a.createElement(r.a,null,l.a.createElement("h1",null,"페이지를 찾을 수 없습니다"),l.a.createElement("p",null,"찾는 페이지가 삭제되었거나 이동되었을 수 있습니다."))))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-ad04b7baa126c6a80163.js.map
