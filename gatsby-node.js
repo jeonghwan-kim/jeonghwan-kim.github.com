8463809d0a2a73ac971ae99fa0fbab7be6de1456
김정환 <ej88ej@gmail.com>
2021-07-20 21:29:39 +0900

    case-sensitive-paths-webpack-plugin 추가

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,6 +4,8 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
+const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin")
+
 const nodes = require("./gatsby/nodes")
 const pages = require("./gatsby/pages")
 const sourceNodes = require("./gatsby/source-nodes")
@@ -21,4 +23,12 @@ exports.createPages = async ({ graphql, actions }) => {
 exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
   sourceNodes.createSeriesNode({ actions, createNodeId, createContentDigest })
   sourceNodes.createVideoNode({ actions, createNodeId, createContentDigest })
+}
+
+exports.onCreateWebpackConfig = ({ actions, stage }) => {
+  if (stage === "develop" || stage === "build-javascript") {
+    actions.setWebpackConfig({
+      plugins: [new CaseSensitivePathsPlugin()],
+    })
+  }
 }
