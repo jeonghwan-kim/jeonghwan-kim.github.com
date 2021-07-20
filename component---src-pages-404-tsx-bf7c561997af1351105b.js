57fe61d0cf0f4eef549e13ac54ce85903a0c34d0
김정환 <ej88ej@gmail.com>
2021-07-20 21:49:20 +0900

    Updates

--- component---src-pages-404-tsx-bf7c561997af1351105b.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"i6+/":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),c=n("Ac47"),r=n("w6rB"),u=n("PIHY"),o=n("BCat");t.default=function(){return l.a.createElement(c.b,null,l.a.createElement(u.a,{title:"404: Not found"}),l.a.createElement(o.c,{small:!0},l.a.createElement(r.a,null,l.a.createElement("h1",null,"페이지를 찾을 수 없습니다"),l.a.createElement("p",null,"찾는 페이지가 삭제되었거나 이동되었을 수 있습니다."))))}}}]);
-//# sourceMappingURL=component---src-pages-404-tsx-bf7c561997af1351105b.js.map
