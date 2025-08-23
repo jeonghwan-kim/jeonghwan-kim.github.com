5cfe3583c9d0edab096f2dd13a9f4fdde1464581
김정환 <ej88ej@gmail.com>
2025-08-23 14:00:39 +0900

    feat: section 여백 조정

--- src/components/Section/style.ts
+++ src/components/Section/style.ts
@@ -2,7 +2,7 @@ import styled from "styled-components"
 import { SpaceUnit } from "../../styles/style-variables"
 
 export const Section = styled.section`
-  padding: ${SpaceUnit(6)} ${SpaceUnit()};
+  padding: ${SpaceUnit(6)} ${SpaceUnit(2)};
 `
 
 export const SectionTitle = styled.h2`
