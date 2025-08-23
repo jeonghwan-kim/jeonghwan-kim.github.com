4690f7edcceeb30d0fd05034c96e908c06c60664
김정환 <ej88ej@gmail.com>
2025-08-23 13:56:50 +0900

    feat: 홈. 아이템 간격

--- src/components/PostList/style.ts
+++ src/components/PostList/style.ts
@@ -1,10 +1,11 @@
 import styled from "styled-components"
-import { Colors, SpaceUnit, Widths } from "../../styles/style-variables"
+import { Colors, SpaceUnit } from "../../styles/style-variables"
 import { PropsWithChildren } from "react"
 
 export const PostList = styled.ul`
   list-style: none;
   padding: 0;
+  margin: 0;
 `
 
 export const PostItem = styled.li<PropsWithChildren>`
@@ -12,12 +13,19 @@ export const PostItem = styled.li<PropsWithChildren>`
     text-decoration: none;
   }
   margin: ${SpaceUnit(4)} 0;
+  &:first-of-type {
+    margin-top: 0;
+  }
+  &:last-of-type {
+    margin-bottom: 0;
+  }
   text-decoration: none;
   display: block;
 `
 
 export const PostTitle = styled.h2`
   color: ${Colors.Foreground2};
+  margin-top: 0;
   margin-bottom: ${SpaceUnit()};
   &:hover {
     color: ${Colors.Primary};
