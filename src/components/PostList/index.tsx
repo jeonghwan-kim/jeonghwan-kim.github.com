bf953f9314be90ad8d062912ff01174910ceb74d
김정환 <ej88ej@gmail.com>
2025-08-23 13:35:34 +0900

    chore: 코드정리

--- src/components/PostList/index.tsx
+++ src/components/PostList/index.tsx
@@ -13,36 +13,42 @@ export interface PostListProps {
 
 const PostList: FC<PostListProps> = ({ posts, renderMeta }) => {
   return (
-    <Styled.PostList id="post-list">
-      {posts.map(post => {
-        const { frontmatter, excerpt } = post
-        const { slug, date, title } = frontmatter
-        return (
-          <Styled.PostItem key={slug}>
-            <Link to={slug}>
-              <Styled.PostTitle className="post-item-title">
-                {title}
-              </Styled.PostTitle>
-              {date && (
-                <Styled.PostMeta>
-                  {renderMeta ? (
-                    renderMeta(post)
-                  ) : (
-                    <time dateTime={date}>{dateFormat(date)}</time>
-                  )}
-                </Styled.PostMeta>
-              )}
-              {excerpt && (
-                <Styled.PostSummary
-                  dangerouslySetInnerHTML={{
-                    __html: excerpt,
-                  }}
-                />
-              )}
-            </Link>
-          </Styled.PostItem>
-        )
-      })}
+    <Styled.PostList>
+      {posts
+        .map(post => {
+          const { frontmatter, excerpt } = post
+          if (!frontmatter) return
+
+          const { slug, date, title } = frontmatter
+          if (!slug || !date || !title) return
+
+          return (
+            <Styled.PostItem key={slug}>
+              <Link to={slug}>
+                <Styled.PostTitle className="post-item-title">
+                  {title}
+                </Styled.PostTitle>
+                {date && (
+                  <Styled.PostMeta>
+                    {renderMeta ? (
+                      renderMeta(post)
+                    ) : (
+                      <time dateTime={date}>{dateFormat(date)}</time>
+                    )}
+                  </Styled.PostMeta>
+                )}
+                {excerpt && (
+                  <Styled.PostSummary
+                    dangerouslySetInnerHTML={{
+                      __html: excerpt,
+                    }}
+                  />
+                )}
+              </Link>
+            </Styled.PostItem>
+          )
+        })
+        .filter(Boolean)}
     </Styled.PostList>
   )
 }
